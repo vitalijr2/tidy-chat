e061417abbb7561d7ed8de50c79be683d3b68380
Witalij Berdinskich <radio_rogal@keemail.me>
2022-10-02 13:41:53 +0000

    Bump slf4j-api from 1.7.36 to 2.0.3

--- src/test/java/io/gitlab/radio_rogal/tidy_chat/bot/BotHandlerSlowTest.java
+++ src/test/java/io/gitlab/radio_rogal/tidy_chat/bot/BotHandlerSlowTest.java
@@ -13,7 +13,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
 @Tag("slow")
 class BotHandlerSlowTest {
 
